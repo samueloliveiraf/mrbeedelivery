f608d5c08070928ac035465f1140bd67de1c9eef
samueloliveiraf <samueldiablo73@gmail.com>
2022-01-06 01:41:02 -0300

    a

--- app_web/settings.py
+++ app_web/settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'media')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
