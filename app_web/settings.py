041d6f5b332691e5e93fa226d578a0ab00757e0a
samueloliveiraf <samueldiablo73@gmail.com>
2022-02-13 16:16:01 -0300

    adicionando ok

--- app_web/settings.py
+++ app_web/settings.py
@@ -111,7 +111,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'media')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
